2337856d06f7e8ce1a192aa0b8f14de9bf9a11fa
b4den <baden@banked.com>
2022-12-06 09:21:18 +0000

    Removed unsafe calls

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ use rsb_derive::Builder;
 use std::sync::Arc;
 
 use chatgpt_rs::client::GPTClient;
-use once_cell::sync::OnceCell;
-
-static mut CLIENT: OnceCell<GPTClient> = OnceCell::new();
 
 // we should dispatch events in a separate thread given Slack proclivity to retry requests upon non non acknowledgement.
 async fn test_interaction_events_function(
@@ -45,10 +42,10 @@ async fn test_command_events_function(
 async fn test_push_events_sm_function(
     event: SlackPushEventCallback,
     client: Arc<SlackHyperClient>,
-    _states: SlackClientEventsUserState,
+    states: SlackClientEventsUserState,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("{:#?}", event);
-    tokio::spawn(async move { process_response(event, client).await });
+    tokio::spawn(async move { process_response(event, client, states).await });
 
     Ok(())
 }
@@ -56,6 +53,7 @@ async fn test_push_events_sm_function(
 async fn process_response(
     event: SlackPushEventCallback,
     client: Arc<SlackHyperClient>,
+    states: SlackClientEventsUserState,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
     let token_value: SlackApiTokenValue = config_env_var("SLACK_TEST_TOKEN")?.into();
     let token: SlackApiToken = SlackApiToken::new(token_value);
@@ -72,29 +70,25 @@ async fn process_response(
             Some(same)
         }
         _ => None,
-        //SlackPushEventCallback { event, .. } => {println!("Got callback {event:?}"),
     };
+
     let message_details = maybe_mention.unwrap();
-    //let query = message_details.content.text.unwrap_or("I did not understand that".to_string())[15..];
     let query = message_details.content.text.clone().expect("Should have content");
+
     if !query.starts_with("<") { return Err("Tag me first, then ask your question".into()) }
     let query = &query[15..];
 
-    //let message = WelcomeMessageTemplateParams::new("".into());
-    let mut answer = unsafe { CLIENT.get_mut().unwrap().post(query.to_string()).await? };
-    println!("Answer is {answer}");
-    //answer = answer.replace("\\n\\n", "\"\n\""); // better
-    //answer = answer.replace("\\n\\n", "\n"); // betterer
+    let state = states.read().await;
+    let user_state = state.get_user_state::<UserState>();
+    let mut cl = user_state.cloned().expect("Could not get GPTClient");
+    let mut answer = cl.client.post(query.to_string()).await?;
+
     answer = answer.replace("\\n", "\n"); // best.
-    println!("Answer after {answer}");
     let message = WelcomeMessageTemplateParams::new(message_details.user.into(),
     query.to_string(), answer);
-    //let post_chat_req =
-    //   SlackApiChatPostMessageRequest::new("#general".into(), message.render_template());
     let post_chat_req =
         SlackApiChatPostMessageRequest::new(message_details.channel, message.render_template());
     session.chat_post_message(&post_chat_req).await?;
-    //session.chat_post_message(SlackApiChatPostMessageResponse::new(channel, ts, message))
     println!("Push event done!");
 
     Ok(())
@@ -113,7 +107,12 @@ fn test_error_handler(
     http::StatusCode::OK
 }
 
-async fn test_client_with_socket_mode() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+#[derive(Debug, Clone)]
+struct UserState {
+    client: GPTClient
+}
+
+async fn test_client_with_socket_mode(gpt_client: GPTClient) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client = Arc::new(SlackClient::new(SlackClientHyperConnector::new()));
 
     let socket_mode_callbacks = SlackSocketModeListenerCallbacks::new()
@@ -123,7 +122,8 @@ async fn test_client_with_socket_mode() -> Result<(), Box<dyn std::error::Error 
 
     let listener_environment = Arc::new(
         SlackClientEventsListenerEnvironment::new(client.clone())
-            .with_error_handler(test_error_handler),
+            .with_error_handler(test_error_handler)
+            .with_user_state(UserState { client: gpt_client }),
     );
 
     let socket_mode_listener = SlackClientSocketModeListener::new(
@@ -148,15 +148,9 @@ pub fn config_env_var(name: &str) -> Result<String, String> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    //let subscriber = tracing_subscriber::fmt()
-    //    .with_env_filter("slack_morphism_hyper=debug,slack_morphism=debug")
-    //    .finish();
-    //tracing::subscriber::set_global_default(subscriber)?;
 
     let gpt_client = GPTClient::new()?;
-    unsafe { CLIENT.set(gpt_client).unwrap() };
-
-    test_client_with_socket_mode().await?;
+    test_client_with_socket_mode(gpt_client).await?;
 
     Ok(())
 }
@@ -180,11 +174,7 @@ impl SlackMessageTemplate for WelcomeMessageTemplateParams {
                 some_into(SlackDividerBlock::new()),
                 some_into(SlackHeaderBlock::new(pt!(&self.question))),
                 some_into(SlackDividerBlock::new()),
-                //some_into(SlackSectionBlock::new().with_text(md!("Hey *user*!"))),
                 some_into(SlackSectionBlock::new().with_text(md!(&self.answer))),
-                //some_into(SlackContextBlock::new(slack_blocks![
-                //    some(md!("This is an example of block message")),
-                //])),
                 some_into(SlackDividerBlock::new()),
                 some_into(SlackActionsBlock::new(slack_blocks![some_into(
                     SlackBlockButtonElement::new(
